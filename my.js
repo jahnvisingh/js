69bd958aaad94d7c2f1f90fa418262fc537c99b2
jahnvisingh <jahnvisingh.1196@gmail.com>
2017-06-11 13:23:36 +0530

    Add files via upload

--- my.js
+++ my.js
@@ -1,16 +1,17 @@
 $(document).ready(function(){
- var selectletter=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+ var selectletter=["A","A","B","B","C","C","D","D","E","E","F","F","G","G","H","H"];
  var counter=0;
  var all=new Array();
  var fun=function()
    {
-   $(this).css({"background-color":"white","text-align":"center","font-size":"50px"}).html(selectletter[Math.floor(Math.random() * 26)]);
-   all[counter]=$(this);
+   $(this).css({"background-color":"white","text-align":"center","font-size":"50px"}).html(selectletter[Math.floor(Math.random() * 16)]);
+   all[counter]=$(this).text();
    counter++;
       if(counter==2)
         { if(all[0]==all[1])
     	   {  
-              alert("success!!");
+             
+             alert("success!!");
            }
           else
            { 
