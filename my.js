8b9a223b83eb77d020149a4855e4032960497e41
jahnvisingh <jahnvisingh.1196@gmail.com>
2017-06-11 12:50:33 +0530

    Add files via upload

--- my.js
+++ my.js
@@ -1,8 +1,25 @@
 $(document).ready(function(){
  var selectletter=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
- 
-$("td").click(function(){
-$(this).css({"background-color":"white","text-align":"center","font-size":"30px"}).append(selectletter[Math.floor(Math.random() * 26)]);
-});
+ var counter=0;
+ var all=new Array();
+ var fun=function()
+   {
+   $(this).css({"background-color":"white","text-align":"center","font-size":"50px"}).html(selectletter[Math.floor(Math.random() * 26)]);
+   all[counter]=$(this);
+   counter++;
+      if(counter==2)
+        { if(all[0]==all[1])
+    	   {  
+              alert("success!!");
+           }
+          else
+           { 
+             
+       	     alert("failure!!");
+       	   }
+       }
+   }
+ $("td").on('click',fun);
+    
 
 });
